a4e87cf4c121894c433222e02507331e2cc4674f
Choccy-vr <94711504+Choccy-vr@users.noreply.github.com>
2025-08-25 22:41:07 -0400

    Website

--- Boot-Web/boot_web/lib/main.dart
+++ Boot-Web/boot_web/lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 import 'theme/terminal_theme.dart';
-import 'pages/Test_Home.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+import 'pages/Home.dart';
+import 'pages/Sponsor.dart';
+
+const supabaseUrl = 'https://zbtphhtuaovleoxkoemt.supabase.co';
+const supabaseKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpidHBoaHR1YW92bGVveGtvZW10Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU0NjU4MDEsImV4cCI6MjA3MTA0MTgwMX0.qogFGForru9M9rutCcMQSNJuGpP46LpLdWo03lvYqMQ';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(url: supabaseUrl, anonKey: supabaseKey);
   runApp(const MainApp());
 }
 
@@ -14,7 +23,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: 'Boot Web',
       theme: buildTerminalTheme(),
-      home: const HomePage(),
+      home: HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
